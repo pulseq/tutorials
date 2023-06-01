86855c180810b54ea8c2cf50061960022b34fa18
Qingping Chen <qingping_chen@hotmail.com>
2023-06-01 09:38:58 +0200

    Update s06_GRE_tutorial_step5.m

--- 02_basic_gradient_echo/seq/s06_GRE_tutorial_step5.m
+++ 02_basic_gradient_echo/seq/s06_GRE_tutorial_step5.m
@@ -124,15 +124,15 @@ title('k-space trajectory (k_x/k_y)');
 
 %%
 
-[ktraj_adc, ktraj, t_excitation, t_refocusing, t_adc] = seq.calculateKspace();
+% [ktraj_adc, ktraj, t_excitation, t_refocusing, t_adc] = seq.calculateKspace();
 
 % plot k-spaces
-time_axis=(1:(size(ktraj,2)))*sys.gradRasterTime;
-figure; plot(time_axis, ktraj'); % plot the entire k-space trajectory
-hold; plot(t_adc,ktraj_adc(1,:),'.'); % and sampling points on the kx-axis
-figure; plot(ktraj(1,:),ktraj(2,:),'b'); % a 2D plot
-axis('equal'); % enforce aspect ratio for the correct trajectory display
-hold;plot(ktraj_adc(1,:),ktraj_adc(2,:),'r.'); % plot the sampling points
+% time_axis=(1:(size(ktraj,2)))*sys.gradRasterTime;
+% figure; plot(time_axis, ktraj'); % plot the entire k-space trajectory
+% hold; plot(t_adc,ktraj_adc(1,:),'.'); % and sampling points on the kx-axis
+% figure; plot(ktraj(1,:),ktraj(2,:),'b'); % a 2D plot
+% axis('equal'); % enforce aspect ratio for the correct trajectory display
+% hold;plot(ktraj_adc(1,:),ktraj_adc(2,:),'r.'); % plot the sampling points
 
 %% very optional step, slow but useful for testing during development e.g. for the real TE, TR or for staying within slewrate limits  
 rep = seq.testReport;
